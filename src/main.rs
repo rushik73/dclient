0e56728ed95a96c85c7eb2cd195e08bfa48fa0ec
rushik guduru <rushikg14@gmail.com>
2022-07-17 16:24:12 -0500

    Getting the address as an argument and printing out the amount of data received.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use reqwest::blocking::Client;
 
 fn main() {
     let client = Client::new();
+    let args = std::env::args();
+    let mut args = args.skip(1);
+    let address = args.next().unwrap();
+    let mut total = 0;
     loop {
-        let size = client
-            .get("https://www.youtube.com")
-            .send()
-            .unwrap()
-            .bytes()
-            .unwrap()
-            .len();
+        let size = client.get(&address).send().unwrap().bytes().unwrap().len();
+        total += size;
+        println!("total: {}", total);
     }
 }
